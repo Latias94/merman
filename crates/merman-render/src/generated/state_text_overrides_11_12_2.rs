92e863849d51d0559a9d3e575ceb7fcaa86ace5e
Latias94 <superfrankie621@gmail.com>
2026-02-01 21:00:48 +0800

    fix(state): align acc descr label width

--- crates/merman-render/src/generated/state_text_overrides_11_12_2.rs
+++ crates/merman-render/src/generated/state_text_overrides_11_12_2.rs
@@ -25,6 +25,8 @@ pub fn lookup_state_node_label_width_px(font_size_px: f64, text: &str) -> Option
     match text {
         // fixtures/upstream-svgs/state/basic.svg
         "Idle" => Some(26.8125),
+        // fixtures/upstream-svgs/state/upstream_stateDiagram_acc_descr_multiline_spec.svg
+        "this is another string" => Some(147.765625),
         // fixtures/upstream-svgs/state/upstream_stateDiagram_spec.svg
         "State2" => Some(45.90625),
         _ => None,
