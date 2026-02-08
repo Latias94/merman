18014c0ecad5cec79fa8cf75e6a920155e738c48
Latias94 <superfrankie621@gmail.com>
2026-02-08 14:22:16 +0800

    fix(render): add architecture root viewport overrides

--- crates/merman-render/src/generated/architecture_root_overrides_11_12_2.rs
+++ crates/merman-render/src/generated/architecture_root_overrides_11_12_2.rs
@@ -9,6 +9,42 @@ pub fn lookup_architecture_root_viewport_override(
     diagram_id: &str,
 ) -> Option<(&'static str, &'static str)> {
     match diagram_id {
+        "upstream_architecture_cypress_complex_junction_edges_normalized" => Some((
+            "-333.2172546386719 -266.3057403564453 746.4345092773438 663.6115112304688",
+            "746.4345092773438",
+        )),
+        "upstream_architecture_cypress_directional_arrows_normalized" => Some((
+            "-195.6711883544922 -187.24441528320312 514.3424072265625 538.5298767089844",
+            "514.3424072265625",
+        )),
+        "upstream_architecture_cypress_edge_labels_normalized" => Some((
+            "-185.4211883544922 -182.99441528320312 514.3424072265625 538.5298767089844",
+            "514.3424072265625",
+        )),
+        "upstream_architecture_cypress_groups_normalized" => Some((
+            "-183.60711669921875 -262.5092315673828 447.2142639160156 660.0184936523438",
+            "447.2142639160156",
+        )),
+        "upstream_architecture_demo_arrow_mesh_bidirectional" => Some((
+            "-195.6711883544922 -187.24441528320312 514.3424072265625 538.5298767089844",
+            "514.3424072265625",
+        )),
+        "upstream_architecture_demo_arrow_mesh_bidirectional_inverse" => Some((
+            "-195.49441528320312 -187.4211883544922 514.3423767089844 538.5299072265625",
+            "514.3423767089844",
+        )),
+        "upstream_architecture_demo_edge_label_long" => Some((
+            "-151.49441528320312 -182.99441528320312 514.3423767089844 538.5298767089844",
+            "514.3423767089844",
+        )),
+        "upstream_architecture_demo_edge_label_short" => Some((
+            "-185.4211883544922 -182.99441528320312 514.3424072265625 538.5298767089844",
+            "514.3424072265625",
+        )),
+        "upstream_architecture_demo_junction_groups_arrows" => Some((
+            "-333.2172546386719 -266.3057403564453 746.4345092773438 663.6115112304688",
+            "746.4345092773438",
+        )),
         _ => None,
     }
 }
