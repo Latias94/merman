17932860b13451383c5b7975735351ef29aa9cde
Latias94 <superfrankie621@gmail.com>
2026-02-14 10:42:38 +0800

    test(timeline): import upstream cypress fixtures (batch 1)

--- crates/merman-render/src/generated/timeline_root_overrides_11_12_2.rs
+++ crates/merman-render/src/generated/timeline_root_overrides_11_12_2.rs
@@ -11,6 +11,12 @@ pub fn lookup_timeline_root_viewport_override(
 ) -> Option<(&'static str, &'static str)> {
     match diagram_id {
         "upstream_long_word_wrap" => Some(("9.6796875 0 961.484375 533.3999938964844", "961.484")),
+        "upstream_cypress_timeline_spec_11_should_render_timeline_with_many_stacked_events_and_proper_ti_011" => {
+            Some(("100 -61 1390 1109.5999755859375", "1390"))
+        }
+        "upstream_cypress_timeline_spec_12_should_render_timeline_with_proper_vertical_line_lengths_for_012" => {
+            Some(("100 -57 2190 879.4000244140625", "2190"))
+        }
         _ => None,
     }
 }
