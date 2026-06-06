1044ff1686ceabf6dbd563612a63a98c1f84788c
Latias94 <superfrankie621@gmail.com>
2026-06-06 11:07:51 +0800

    fix(python): build native wheels as platlib

--- platforms/python/merman/setup.py
+++ platforms/python/merman/setup.py
@@ -1,5 +1,10 @@
-from setuptools import setup
-from wheel.bdist_wheel import bdist_wheel
+from setuptools import Distribution, setup
+from setuptools.command.bdist_wheel import bdist_wheel
+
+
+class BinaryDistribution(Distribution):
+    def has_ext_modules(self):
+        return True
 
 
 class PlatformWheel(bdist_wheel):
@@ -12,4 +17,4 @@ class PlatformWheel(bdist_wheel):
         return "py3", "none", platform_tag
 
 
-setup(cmdclass={"bdist_wheel": PlatformWheel})
+setup(distclass=BinaryDistribution, cmdclass={"bdist_wheel": PlatformWheel})
